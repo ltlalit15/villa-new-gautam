eebf45f0503864045d655ddc2d4be55b4f322f35
GautamBairagi <gauatambairagi22@gmail.com>
2025-11-17 20:15:19 +0530

    sadddadda

--- Config/dbConnect.js
+++ Config/dbConnect.js
@@ -1,42 +1,13 @@
-// import { JSONCookie } from 'cookie-parser';
-// import { query } from 'express';
-// import mysql from 'mysql2/promise';
-// export const pool = mysql.createPool({
-//   host: "gondola.proxy.rlwy.net",       // ✅ Updated Host
-//   port: 53923,                           // ✅ Updated Port
-//   user: "root",                          // ✅ Username
-//   password: "ayYnhfmRlqfMcIlYXfxPPdjgJAZrAuUC", // ✅ Password
-//   database: "railway",                  // ✅ Database Name
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-//   connectTimeout: 10000,
-// });
-// // Check connection
-// (async () => {
-//   try {
-//     const connection = await pool.getConnection();
-//     console.log("✅ Successfully connected to Railway MySQL database");
-//     connection.release();
-//   } catch (error) {
-//     console.error("❌ Database connection error:", error);
-//   }
-// })();
-
-
-
-
-
 
 import { JSONCookie } from 'cookie-parser';
 import { query } from 'express';
 import mysql from 'mysql2/promise';
 export const pool = mysql.createPool({
-  host: "localhost",            // ✅ Match CLI host
-  port: 3306,                                // ✅ Match CLI port
+  host: "gondola.proxy.rlwy.net",            // ✅ Match CLI host
+  port: 23390,                                // ✅ Match CLI port
   user: "root",                               // ✅ Match CLI user
-  password: "", // ✅ Match CLI password
-  database: "villa-booking",                        // ✅ Match CLI DB name
+  password: "mDrJzTKDIBGVDGlBvFKQTajqLBKsZwZO", // ✅ Match CLI password
+  database: "railway",                        // ✅ Match CLI DB name
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
@@ -53,3 +24,6 @@ export const pool = mysql.createPool({
     console.error("❌ Database connection error:", error);
   }
 })();
+
+
+
